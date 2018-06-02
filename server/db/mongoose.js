b79b5b1a70b2fd54d028cfa96e99e13f88ebe9fd
jt1992@gmail.com <jt1992@gmail.com>
2018-06-02 16:44:55 +0800

    clean up config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-console.log(process.env.MONGOLAB_URI);
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://jt.houk:X63sB\!DGuhRyycr*@ds245250.mlab.com:45250/playground');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose }
