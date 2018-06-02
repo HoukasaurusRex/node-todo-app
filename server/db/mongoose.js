eee559b5614ac09aa35ef6cd548df206873824d9
jt1992@gmail.com <jt1992@gmail.com>
2018-06-02 16:43:18 +0800

    config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-console.log(process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://jt.houk:X63sB\!DGuhRyycr*@ds245250.mlab.com:45250/playground');
+console.log(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://jt.houk:X63sB\!DGuhRyycr*@ds245250.mlab.com:45250/playground');
 
 module.exports = { mongoose }
