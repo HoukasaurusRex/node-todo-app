ff4847533bec1adb7a58402c7d4b290232ed9d9c
jt1992@gmail.com <jt1992@gmail.com>
2018-06-06 21:41:06 +0800

    created user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,18 +1,46 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     minLength: 1,
-    trim: true
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
   },
   password: {
     type: String,
     required: true,
-    minLength: 1,
-    trim: true
-  }
+    minLength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
+
+UserSchema.methods.generateAuthToken = function() {
+  const user = this;
+  const access = 'auth';
+  const token = jwt.sign({_id: user._id.toHexString(), access}, 'secretsauce').toString();
+
+  user.tokens.push({ access, token });
+  console.log('Saving user...', user);
+  return user.save().then(() => token);
+};
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = { User }
--- server/server.js
+++ server/server.js
@@ -89,7 +89,20 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((err) => {
     res.status(400).send(err);
   })
-})
+});
+
+app.post('/users', (req, res) => {
+  console.log('Creating new user...');
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port} in ${env} mode`);
